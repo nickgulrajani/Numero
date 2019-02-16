5a9294e86f4dc06198d597cc7f4da7f415f0ce1c
mataharimau <audrey.tam@gmail.com>
2019-02-17 09:10:37 +1100

    Test for I: expect fail

---
+++ NumeroTests/ConverterTests.swift
@@ -0,0 +1,29 @@
+//
+//  ConverterTests.swift
+//  NumeroTests
+//
+//  Created by Audrey Tam on 16/2/19.
+//  Copyright © 2019 Facebook. All rights reserved.
+//
+
+import XCTest
+@testable import Numero
+
+class ConverterTests: XCTestCase {
+  var converter: Converter!
+  
+  override func setUp() {
+    super.setUp()
+    converter = Converter()
+  }
+  
+  override func tearDown() {
+    converter = nil
+    super.tearDown()
+  }
+  
+  func testConversionForOne() {
+    let result = converter.convert(1)
+    XCTAssertEqual(result, "I", "Conversion for 1 is incorrect")
+  }
+}
